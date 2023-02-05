9476b5e50a70e17f6f7d00410caad3bdae5513b7
papadoster <papadoster13@gmail.com>
2023-02-05 02:18:47 +0100

    init commit

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    let imagePickerLibary = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        imagePickerLibary.delegate = self
+        imagePickerLibary.sourceType = .photoLibrary
+        imagePickerLibary.allowsEditing = false
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePickerLibary.dismiss(animated: true)
+        imagePicker.dismiss(animated: true)
+    }
+    
+    
+    @IBAction func libaryTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePickerLibary, animated: true)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true)
+    }
+    
 }
 
